69ec95dcd406b4be6cb4f23c9ccf6d49029f8ef4
paulasuncion <paulyasuncion1@gmail.com>
2022-11-20 15:38:28 -0800

    Update label_maker.js

--- ObjectScripts/label_maker.js
+++ ObjectScripts/label_maker.js
@@ -1,41 +1,41 @@
-// let myInfo = {
-//     fullName: 'Paul Asuncion',
-//     address: '268 Oriente St',
-//     city: 'daly city',
-//     state: 'California',
-//     zip: '94014',
-// }
-
-// function printContact(contact) {
-//     let printContact = `${contact.fullName} ${contact.address} ${contact.city}, ${contact.state} ${contact.zip}`;
-//     console.log(printContact)
-// }
-
-// printContact(myInfo)
-
-let firstLine = {
-    fullName: 'Paul Asuncion'
-}
-let secondLine = {
-    address: '268 Oriente st'
-}
-let thirdLine = {
+let myInfo = {
+    fullName: 'Paul Asuncion',
+    address: '268 Oriente St',
     city: 'daly city',
-    state: 'california',
+    state: 'California',
     zip: '94014',
 }
 
-function printContact(mail1) {
-    let printContact = `${fullName}`;
-    console.log(printContact)
-}
-function printContact(mail2) {
-    let printContact = `${address}`;
-    console.log(printContact)
-}
-function printContact(mail3) {
-    let printContact = `${city} ${state} ${zip}`;
+function printContact(contact) {
+    let printContact = `${contact.fullName} ${contact.address} ${contact.city}, ${contact.state} ${contact.zip}`;
     console.log(printContact)
 }
 
-printContact(mail1, mail2, mail3)
+printContact(myInfo)
+
+// let firstLine = {
+//     fullName: 'Paul Asuncion'
+// }
+// let secondLine = {
+//     address: '268 Oriente st'
+// }
+// let thirdLine = {
+//     city: 'daly city',
+//     state: 'california',
+//     zip: '94014',
+// }
+
+// function printContact(mail1) {
+//     let printContact = `${fullName}`;
+//     console.log(printContact)
+// }
+// function printContact(mail2) {
+//     let printContact = `${address}`;
+//     console.log(printContact)
+// }
+// function printContact(mail3) {
+//     let printContact = `${city} ${state} ${zip}`;
+//     console.log(printContact)
+// }
+
+// printContact(mail1, mail2, mail3)
