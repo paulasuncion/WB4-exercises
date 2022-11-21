03c0fdbf5db3ce9a78f2b617144471d49b6abbda
paulasuncion <paulyasuncion1@gmail.com>
2022-11-21 08:59:17 -0800

    Create avg_scores.js

---
+++ ArrayScripts/avg_scores.js
@@ -0,0 +1,18 @@
+let myScores = [92, 98, 84, 76, 89, 99, 100];
+let yourScores = [82, 98, 94, 88, 92, 100, 100];
+let totalMyScores = myScores.length;
+let sum = 0;
+
+
+function getAverage(total) {
+    for(i = 0; i < totalMyScores; i++);
+        sum += myScores[i];
+        return sum;
+}
+   
+    console.log(total[i])
+
+//find average score in both arrays.
+//to find average: loop through and add up all the numbers in the array
+//the divide by the length of the array
+//return the average
